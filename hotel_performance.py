1278f61bb2107d74f992492d9e9965c2c14f41d0
Gileesa McCormack <gileesa@icloud.com>
2026-05-01 12:43:28 +0200

    feat: enhance hotel performance feature extraction with Bayesian smoothing

--- hotel_performance.py
+++ hotel_performance.py
@@ -26,24 +26,28 @@ def extract_hotel_performance_train(train_df):
     - train_df (pd.DataFrame): the train dataframe with new features
     '''
 
-    # Get global stats
+    # Get row-wise stats
     total_bookings = train_df.groupby('prop_id')['booking_bool'].transform('sum')
     total_position = train_df.groupby('prop_id')['position'].transform('sum')
     total_clicks = train_df.groupby('prop_id')['click_bool'].transform('sum')
     total_count = train_df.groupby('prop_id')['booking_bool'].transform('count')
 
-    # Compute features, leaving out current row
-    train_df['hotel_booking_rate'] = (total_bookings - train_df['booking_bool']) / (total_count - 1)
-    train_df['hotel_click_rate'] = (total_clicks - train_df['click_bool']) / (total_count - 1)
-    train_df['hotel_avg_position'] = (total_position - train_df['position']) / (total_count - 1)
-    train_df['hotel_n_appearances'] = total_count - 1
-
-    # Hotels appearing only once (= no history) get NaN; impute global mean instead
-    # NOTE: global mean seems biased?
+    # Get global stats
+    global_position_avg = train_df['position'].mean()
     global_booking_rate = train_df['booking_bool'].mean()
     global_click_rate = train_df['click_bool'].mean()
-    train_df['hotel_booking_rate'] = train_df['hotel_booking_rate'].fillna(global_booking_rate)
-    train_df['hotel_click_rate'] = train_df['hotel_click_rate'].fillna(global_click_rate)
+
+    # Compute features, leaving out current row (to avoid bias)
+    # Apply bayesian smoothing: https://en.wikipedia.org/wiki/Bayesian_average 
+    minusone = total_count - 1
+    C_bayesian = 30
+
+    train_df['hotel_booking_rate'] = ((total_bookings - train_df['booking_bool']) + C_bayesian * global_booking_rate ) / (minusone + C_bayesian)
+    train_df['hotel_click_rate'] = ((total_clicks - train_df['click_bool']) + C_bayesian * global_click_rate) / (minusone + C_bayesian)
+    train_df['hotel_avg_position'] = ((total_position - train_df['position']) + C_bayesian *  global_position_avg)/ (C_bayesian + minusone)
+    train_df['hotel_n_appearances'] = minusone
+
+    #NOTE: bayesian smoothing: https://jedleee.medium.com/bayesian-laplace-smoothing-applications-in-modern-machine-learning-ef6c38153940 
 
 def extract_hotel_performance_test(train_df, test_df):
     '''
